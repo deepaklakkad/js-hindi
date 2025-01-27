957eac5f3563f2b5d01e9e56132bb9c837f7f484
Deepak Lakkad <deepak.lakkad@gmail.com>
2025-01-27 03:52:43 +0000

    variables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,13 @@
+const accountId = 199621147
+let accountEmail = "deepak@gmail.com"
+var accountPassword = "12345"
+accountCity = "Ahmedabad"
+let accountState
+
+// accountId = 2    // not allowed
+accountEmail = "dl@divinesystems.in"
+accountPassword ="199621147"
+accountCity = "Amreli"
+
+//console.log(accountId);
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState])
