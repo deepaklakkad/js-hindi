6a1e2943bdb5b25495b6d09427b651bddf7a1109
Deepak Lakkad <deepak.lakkad@gmail.com>
2025-02-17 17:33:44 +0000

    datatypes

--- 01_basics/02_DataTypes.js
+++ 01_basics/02_DataTypes.js
@@ -1,1 +1,23 @@
-alert("Hello")
+"use strict"; // treat all JS code as newer version
+
+// alert(3 + 3) // wer are using nodejs, not browser
+
+console.log(3 + 3)
+
+console.log("Deepak")
+
+let age = 18
+let isLoggedIn = false
+let state;
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null
+// undefined => 
+// symbol => unique
+
+// object
+
+console.log(typeof null)
