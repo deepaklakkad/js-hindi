02d96edde2edb28fd71e221502997bbc52d72e1e
Deepak Lakkad <deepak.lakkad@gmail.com>
2025-02-18 03:41:42 +0000

    conversion of datatypes

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,30 @@
+// let score = "deepak"
+
+// console.log(typeof score)
+// console.log(typeof(score))
+
+// let valueInNumber = Number(score)
+
+// console.log(typeof valueInNumber)
+// console.log(valueInNumber)
+
+// "33" => 33
+// "33abc" >= NaN
+// true => 1; false => 0
+
+let isLoggedIn = "deepak"
+
+let booleanInLoggedIn = Boolean(isLoggedIn)
+
+// console.log(typeof booleanInLoggedIn)
+// console.log(booleanInLoggedIn)
+
+// 1 => true; 0 => false
+// "" => false
+// "deepak" => true
+
+let someNumber = 33
+let stringNumber = String(someNumber)
+
+console.log(someNumber)
+console.log(typeof someNumber)
